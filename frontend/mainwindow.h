ad080afd91a9e9e87bf4a517c17344db4d1903ea
Victor Campos <victormscampos@uol.com.br>
2026-02-25 15:02:41 -0800

    main header file modified with all our future necessary functions

--- frontend/mainwindow.h
+++ frontend/mainwindow.h
@@ -1,6 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -9,15 +16,61 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
+class MainWindow : public QObject
 {
     Q_OBJECT
 
+    // C++ Weather properties in QML
+    Q_PROPERTY(QString city READ city NOTIFY dataChanged)
+    Q_PROPERTY(QString temperature READ temperature NOTIFY dataChanged)
+    Q_PROPERTY(QString condition READ condition NOTIFY dataChanged)
+    Q_PROPERTY(QString description READ description NOTIFY dataChanged)
+    Q_PROPERTY(QString humidity READ humidity NOTIFY dataChanged)
+    Q_PROPERTY(QString feelsLike READ feelsLike NOTIFY dataChanged)
+    Q_PROPERTY(QString suggestion READ suggestion NOTIFY dataChanged)
+    Q_PROPERTY(QVariantList events READ events NOTIFY dataChanged)
+    Q_PROPERTY(bool loading READ loading NOTIFY loadingChanged)
+
+
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QObject *parent = nullptr); //constructor -> *parent = nullptr to avoid memory leaks
+
+    //Getters
+    QString getCity() const {return m_city;}
+    QString getTemp() const {return m_temperature;}
+    QString getCondition() const{return m_condition;}
+    QString getHumidity() const{ return m_humidity;}
+    QString getFeelsLike() const { return m_feelsLike; }
+    QString getSuggestion() const { return m_suggestion; }
+    QVariantList getEvents() const { return m_events; }
+    bool getLoading() const { return m_loading; }
+
+    Q_INVOKABLE void fetchData(); //bridge between c++ and qml
+
+
     ~MainWindow();
 
+signals:
+    void dataChanged();
+    void loadingChanged();
+
+private slots:
+    void onDataReceived(QNetworkReply *reply); // contains the data from Flask in JSON
+
 private:
-    Ui::MainWindow *ui;
+
+    QNetworkAccessManager *m_manager; // this will call our flask flask server
+
+    QString m_city;
+    QString m_temperature;
+    QString m_condition;
+    QString m_description;
+    QString m_humidity;
+    QString m_feelsLike;
+    QString m_suggestion;
+    QVariantList m_events;
+    bool m_loading = false;
+
+
 };
 #endif // MAINWINDOW_H
